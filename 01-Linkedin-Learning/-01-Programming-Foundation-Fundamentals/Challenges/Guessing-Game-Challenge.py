d7e2fc9567c5bfdab49351f1fc3287de8d7b8daa
Mahmoud_Hamza <86957735+Mahmoud-Hamza-Git@users.noreply.github.com>
2023-07-23 21:11:25 +0300

    Update Guessing-Game-Challenge.py

--- 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
+++ 01-Linkedin-Learning/-01-Programming-Foundation-Fundamentals/Challenges/Guessing-Game-Challenge.py
@@ -1,1 +1,9 @@
 
+userinput = input("What's my favorite food? ")
+
+if userinput == "chicken":
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That’s not it!")
+
+print("Thanks for playing!")
